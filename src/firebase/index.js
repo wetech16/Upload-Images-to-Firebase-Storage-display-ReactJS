4104ad208c6cdbe0cd764898eb8ab42f87c297f7
Stephen Guo <wetech16@gmail.com>
2020-10-15 01:06:32 -0700

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,14 +2,14 @@ import firebase from "firebase/app";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBfOh7yoqgGMrMsTxoKoMhP6_9bkUDP0i0",
-  authDomain: "fir-react-upload-5daa4.firebaseapp.com",
-  databaseURL: "https://fir-react-upload-5daa4.firebaseio.com",
-  projectId: "fir-react-upload-5daa4",
-  storageBucket: "fir-react-upload-5daa4.appspot.com",
-  messagingSenderId: "320567469168",
-  appId: "1:320567469168:web:32ea5afe821f2b2cc800b5",
-  measurementId: "G-BLDN7GB52Y"
+  apiKey: "AIzaSyA7em91ttdAmG2r2Y32TvRWzEroKfmU3OM",
+  authDomain: "coupon-7c1b6.firebaseapp.com",
+  databaseURL: "https://coupon-7c1b6.firebaseio.com",
+  projectId: "coupon-7c1b6",
+  storageBucket: "coupon-7c1b6.appspot.com",
+  messagingSenderId: "78733956764",
+  appId: "1:78733956764:web:b761260b0bcb70efc1bf59",
+  measurementId: "G-PYHEG8F9WT",
 };
 
 firebase.initializeApp(firebaseConfig);
